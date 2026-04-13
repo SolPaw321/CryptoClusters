891cd4cf48e88da798859768d142a19263b0ad1b
SolPaw321 <pawel.solarczyk@proton.me>
2026-04-13 14:03:33 +0200

    build: make smoother paths handling

--- src/google_drive/folders_ids.py
+++ src/google_drive/folders_ids.py
@@ -1,6 +1,35 @@
+from typing import  ClassVar
+
+
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 
-CRYPTO_CLUSTERS_FOLDER_ID = '11jBUISmCDUP3nrGm1zKW2AryCnPwWGZv'
-ADDRESSES_FOLDER_ID = '1A38QjfXBWWQsOK8wKxCq3learcqLEh1-'
-SINK_FOLDER_ID = '1odWNnT9Xq0GpdqQ6zvYn5AooaMqcGoT0'
+class GoogleDriveFolderIDs:
+
+    _instance: ClassVar["GoogleDriveFolderIDs | None"] = None
+    _initialized: ClassVar[bool] = False
+
+    def __init__(self) -> None:
+        if self.__class__._initialized:
+            return
+
+        # root
+        self.CRYPTO_CLUSTERS = '11jBUISmCDUP3nrGm1zKW2AryCnPwWGZv'
+        # root/...
+        self.ADDRESSES = '1A38QjfXBWWQsOK8wKxCq3learcqLEh1-'
+        # root/...
+        self.SINK = '1odWNnT9Xq0GpdqQ6zvYn5AooaMqcGoT0'
+
+        self.__class__._initialized = True
+
+    def __new__(cls) -> "GoogleDriveFolderIDs":
+        """
+        Create or return the single instance of the class.
+
+        :return: The singleton instance of the class.
+        """
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+GdPaths = GoogleDriveFolderIDs()
